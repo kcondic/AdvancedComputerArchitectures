6dd4b47b713c22ca2c3fd344fb631df39fe1b1cf
Kreso <kreso@dump.hr>
2019-07-02 01:28:52 +0200

    Added caching project

---
+++ Cache/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            for (var size = 256; size <= 64 * 1024 * 1024; size *= 2)
+            {
+                Console.Write($"{size/256} kB ");
+                TryCache(size);
+            }
+            Console.WriteLine("Done");
+        }
+
+        static void TryCache(int size)
+        {
+            var array = new int[size];
+            const int steps = 64 * 1024 * 1024;
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            for (var i = 0; i < steps; i++)
+                array[i * 16 % size]++;
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
